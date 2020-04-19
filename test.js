31672ce700de7f254194cac5f00638d1ed6d546a
Luan Tao <tvanluanst@gmail.com>
2020-04-19 17:17:28 +0700

    update event signin

---
+++ test.js
@@ -0,0 +1,13 @@
+ async function call ( ){
+    console.log(2)
+    set()
+    console.log(3)
+}
+function set(){
+    kq = 0;
+    for(i=0;i<= 1000000000;i++){
+        kq++
+    }
+    console.log(kq)
+}
+call()
